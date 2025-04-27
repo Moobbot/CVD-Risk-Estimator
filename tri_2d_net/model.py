8231598270fd6cfc098d2fb2801f18effbb83843
Tam Ngo <90234479+Moobbot@users.noreply.github.com>
2025-04-27 20:44:25 +0700

    Add DICOM processing script and update .gitignore files
    - Introduced `dicom_detect_old.py` for reading DICOM images, preprocessing, and predicting CVD risk.
    - Added .gitignore files in `results` and `visualizations` directories to exclude unnecessary files from version control.
    - Updated model initialization paths and improved logging for better traceability.
    - Enhanced heart detection methods and integrated model loading functionalities for improved performance.

--- tri_2d_net/model.py
+++ tri_2d_net/model.py
@@ -16,31 +16,35 @@ import torch.optim as optim
 import torch.utils.data as tordata
 from mpl_toolkits.axes_grid1 import ImageGrid
 from scipy.ndimage import gaussian_filter
+
 # from apex import amp
 from scipy.special import softmax
 from skimage.transform import resize as imresize
 
+sys.path.append("./")
+
 from old_code.data import SoftmaxSampler
-from net import Tri2DNet, Branch
+from tri_2d_net.net import Tri2DNet, Branch
 from tri_2d_net.visualization import GradCam
 
 
 class Model:
     def __init__(
-            self,
-            dout,
-            lr,
-            num_workers,
-            batch_size,
-            restore_iter,
-            total_iter,
-            save_name,
-            model_name,
-            train_source,
-            val_source,
-            test_source,
-            accumulate_steps,
-            prt_path, ):
+        self,
+        dout,
+        lr,
+        num_workers,
+        batch_size,
+        restore_iter,
+        total_iter,
+        save_name,
+        model_name,
+        train_source,
+        val_source,
+        test_source,
+        accumulate_steps,
+        prt_path,
+    ):
 
         self.dout = dout
         self.lr = lr
@@ -57,7 +61,7 @@ class Model:
         self.prt_path = prt_path
 
         encoder = Tri2DNet(dout=self.dout).cuda()
-        ce = nn.CrossEntropyLoss(reduction='none').cuda()
+        ce = nn.CrossEntropyLoss(reduction="none").cuda()
 
         att_id = []
         aux_id = []
@@ -66,19 +70,18 @@ class Model:
                 att_id += list(map(id, m.att_branch.parameters()))
                 aux_id += list(map(id, m.aux.parameters()))
         pretrained_params = filter(
-            lambda p: id(p) not in att_id + aux_id,
-            encoder.parameters())
-        aux_params = filter(
-            lambda p: id(p) in aux_id,
-            encoder.parameters())
-        att_params = filter(
-            lambda p: id(p) in att_id,
-            encoder.parameters())
-        optimizer = optim.Adam([
-            {'params': pretrained_params, 'lr': self.lr / 10},
-            {'params': aux_params, 'lr': self.lr / 5},
-            {'params': att_params, 'lr': self.lr},
-        ], lr=self.lr)
+            lambda p: id(p) not in att_id + aux_id, encoder.parameters()
+        )
+        aux_params = filter(lambda p: id(p) in aux_id, encoder.parameters())
+        att_params = filter(lambda p: id(p) in att_id, encoder.parameters())
+        optimizer = optim.Adam(
+            [
+                {"params": pretrained_params, "lr": self.lr / 10},
+                {"params": aux_params, "lr": self.lr / 5},
+                {"params": att_params, "lr": self.lr},
+            ],
+            lr=self.lr,
+        )
 
         models = [encoder, ce]
         # models, optimizer = amp.initialize(models, optimizer, opt_level="O1")
@@ -108,7 +111,8 @@ class Model:
         train_loader = tordata.DataLoader(
             dataset=self.train_source,
             batch_sampler=softmax_sampler,
-            num_workers=self.num_workers)
+            num_workers=self.num_workers,
+        )
 
         _time1 = datetime.now()
         for volumes, labels in train_loader:
@@ -117,15 +121,18 @@ class Model:
             self.restore_iter += 1
             self.optimizer.zero_grad()
 
-            (pred, aux_pred_sagittal, aux_pred_coronal,
-             aux_pred_axial) = self.encoder(volumes.cuda())
+            (pred, aux_pred_sagittal, aux_pred_coronal, aux_pred_axial) = self.encoder(
+                volumes.cuda()
+            )
 
             labels = (labels > 0).int()
             main_ce_loss = self.ce(pred, labels.cuda().long()).mean()
             sagittal_ce_loss = self.ce(aux_pred_sagittal, labels.cuda().long()).mean()
             axial_ce_loss = self.ce(aux_pred_axial, labels.cuda().long()).mean()
             coronal_ce_loss = self.ce(aux_pred_coronal, labels.cuda().long()).mean()
-            total_loss = (main_ce_loss + sagittal_ce_loss + axial_ce_loss + coronal_ce_loss) / 4
+            total_loss = (
+                main_ce_loss + sagittal_ce_loss + axial_ce_loss + coronal_ce_loss
+            ) / 4
             _total_loss = total_loss.cpu().data.numpy()
             self.loss.append(_total_loss)
             self.m_loss.append(main_ce_loss.cpu().data.numpy())
@@ -147,14 +154,19 @@ class Model:
                 print(datetime.now() - _time1)
                 _time1 = datetime.now()
                 self.save_model()
-                print('iter {}:'.format(self.restore_iter), end='')
-                print(', loss={0:.8f}'.format(np.mean(self.loss)), end='')
-                print(', m_loss={0:.8f}'.format(np.mean(self.m_loss)), end='')
-                print(', sa_loss={0:.8f}'.format(np.mean(self.sa_loss)), end='')
-                print(', co_loss={0:.8f}'.format(np.mean(self.co_loss)), end='')
-                print(', ax_loss={0:.8f}'.format(np.mean(self.ax_loss)), end='')
-                print(', lr=', end='')
-                print([self.optimizer.param_groups[i]['lr'] for i in range(len(self.optimizer.param_groups))])
+                print("iter {}:".format(self.restore_iter), end="")
+                print(", loss={0:.8f}".format(np.mean(self.loss)), end="")
+                print(", m_loss={0:.8f}".format(np.mean(self.m_loss)), end="")
+                print(", sa_loss={0:.8f}".format(np.mean(self.sa_loss)), end="")
+                print(", co_loss={0:.8f}".format(np.mean(self.co_loss)), end="")
+                print(", ax_loss={0:.8f}".format(np.mean(self.ax_loss)), end="")
+                print(", lr=", end="")
+                print(
+                    [
+                        self.optimizer.param_groups[i]["lr"]
+                        for i in range(len(self.optimizer.param_groups))
+                    ]
+                )
                 sys.stdout.flush()
 
                 self.LOSS.append(np.mean(self.loss))
@@ -169,19 +181,20 @@ class Model:
                 plt.plot(self.LOSS)
                 plt.show()
 
-    def aug_test(self, subset='test', batch_size=1):
+    def aug_test(self, subset="test", batch_size=1):
         self.encoder.eval()
-        assert subset in ['train', 'val', 'test']
+        assert subset in ["train", "val", "test"]
         source = self.test_source
-        if subset == 'train':
+        if subset == "train":
             source = self.train_source
-        elif subset == 'val':
+        elif subset == "val":
             source = self.val_source
         data_loader = tordata.DataLoader(
             dataset=source,
             batch_size=batch_size,
             sampler=tordata.sampler.SequentialSampler(source),
-            num_workers=self.num_workers)
+            num_workers=self.num_workers,
+        )
 
         pred_list = list()
         label_list = list()
@@ -207,10 +220,11 @@ class Model:
                     s = _c[0]
                     h = _c[1]
                     w = _c[2]
-                    _v.append(volumes[:, :, s:s + 112, h:h + 112, w:w + 112])
+                    _v.append(volumes[:, :, s : s + 112, h : h + 112, w : w + 112])
                 _v = torch.cat(_v, 0).contiguous()
-                (pred, aux_pred_sagittal, aux_pred_coronal,
-                 aux_pred_axial) = self.encoder(_v)
+                (pred, aux_pred_sagittal, aux_pred_coronal, aux_pred_axial) = (
+                    self.encoder(_v)
+                )
                 pred = pred.view(len(crop), b_s, 2)
                 pred_prob = softmax(pred.data.cpu().numpy(), axis=2).mean(axis=0)
                 pred_list.append(pred_prob)
@@ -246,10 +260,11 @@ class Model:
                 s = _c[0]
                 h = _c[1]
                 w = _c[2]
-                _v.append(volumes[:, :, s:s + 112, h:h + 112, w:w + 112])
+                _v.append(volumes[:, :, s : s + 112, h : h + 112, w : w + 112])
             _v = torch.cat(_v, 0).contiguous()
-            (pred, aux_pred_sagittal, aux_pred_coronal,
-             aux_pred_axial) = self.encoder(_v)
+            (pred, aux_pred_sagittal, aux_pred_coronal, aux_pred_axial) = self.encoder(
+                _v
+            )
             pred = pred.view(len(crop), 2)
             pred_prob = softmax(pred.data.cpu().numpy(), axis=1).mean(axis=0)
 
@@ -263,10 +278,10 @@ class Model:
 
         color = cv2.COLORMAP_JET
         color_sample = np.asarray(list(range(0, 10)) * 3).reshape(3, 10)
-        color_sample = imresize(color_sample.astype('float'), (12, 128))
+        color_sample = imresize(color_sample.astype("float"), (12, 128))
         color_sample = color_sample / 10
         color_sample = cv2.applyColorMap(np.uint8(255 * color_sample), color)
-        color_sample = cv2.cvtColor(color_sample,cv2.COLOR_BGR2RGB)
+        color_sample = cv2.cvtColor(color_sample, cv2.COLOR_BGR2RGB)
         plt.imshow(color_sample)
         plt.yticks(np.arange(0))
         plt.xticks(np.arange(-1, 128, 32), [0, 0.25, 0.5, 0.75, 1.0])
@@ -276,7 +291,7 @@ class Model:
             img = np.float32(img) / 255
             heatmap = cv2.applyColorMap(np.uint8(255 * mask), color)
             heatmap = np.float32(heatmap) / 255
-            cam = heatmap * .5 + np.float32(img) * .5
+            cam = heatmap * 0.5 + np.float32(img) * 0.5
             cam = np.clip(cam * 1.1 + 0.15, 0, 1)
             cam = np.uint8(255 * cam)
             return cam
@@ -285,7 +300,7 @@ class Model:
             weight = grad.mean(dim=(2, 3))
             s, c = weight.size()
             cam = F.relu((weight.view(s, c, 1, 1) * output).sum(dim=1))
-            cam = cam.data.cpu().numpy().astype('float')
+            cam = cam.data.cpu().numpy().astype("float")
             cam = imresize(cam, (128, 128, 128))
             return cam
 
@@ -298,9 +313,14 @@ class Model:
         y = (one_hot * pred).sum()
         y.backward()
 
-        (axial_output, coronal_output, sagittal_output,
-         axial_grad, coronal_grad, sagittal_grad,
-         ) = grad_cam.get_intermediate_data()
+        (
+            axial_output,
+            coronal_output,
+            sagittal_output,
+            axial_grad,
+            coronal_grad,
+            sagittal_grad,
+        ) = grad_cam.get_intermediate_data()
         # axial: d, h, w
         axial_cam = v_2D(axial_output, axial_grad[0])
         axial_cam = gaussian_filter(axial_cam, sigma=(3, 0, 0))
@@ -314,7 +334,9 @@ class Model:
         sagittal_cam = gaussian_filter(sagittal_cam, sigma=(0, 0, 3))
 
         cam_combine = axial_cam + coronal_cam + sagittal_cam
-        cam_combine = (cam_combine - cam_combine.min()) / (cam_combine.max() - cam_combine.min() + 1e-9)
+        cam_combine = (cam_combine - cam_combine.min()) / (
+            cam_combine.max() - cam_combine.min() + 1e-9
+        )
 
         _v = volumes.data.numpy()[0][0]
         total_img_num = _v.shape[0]
@@ -322,7 +344,9 @@ class Model:
         grid = ImageGrid(fig, 111, nrows_ncols=(32, 2), axes_pad=0.05)
         for i in range(64):
             frame_dix = i * int(total_img_num / 64)
-            org_img = cv2.cvtColor(np.uint8(255 * _v[frame_dix].reshape(128, 128)), cv2.COLOR_GRAY2BGR)
+            org_img = cv2.cvtColor(
+                np.uint8(255 * _v[frame_dix].reshape(128, 128)), cv2.COLOR_GRAY2BGR
+            )
             merged = show_cam_on_image(org_img, cam_combine[frame_dix])
             coupled = np.concatenate([org_img, merged], axis=1)
             coupled = cv2.cvtColor(coupled, cv2.COLOR_BGR2RGB)
@@ -330,22 +354,35 @@ class Model:
         plt.show()
 
     def save_model(self):
-        torch.save(self.encoder.state_dict(), osp.join(
-            'checkpoint',
-            '{}-{:0>5}-encoder.ptm'.format(self.save_name, self.restore_iter)))
-        torch.save(self.optimizer.state_dict(), osp.join(
-            'checkpoint',
-            '{}-{:0>5}-optimizer.ptm'.format(self.save_name, self.restore_iter)))
+        torch.save(
+            self.encoder.state_dict(),
+            osp.join(
+                "checkpoint",
+                "{}-{:0>5}-encoder.ptm".format(self.save_name, self.restore_iter),
+            ),
+        )
+        torch.save(
+            self.optimizer.state_dict(),
+            osp.join(
+                "checkpoint",
+                "{}-{:0>5}-optimizer.ptm".format(self.save_name, self.restore_iter),
+            ),
+        )
 
     def load_model(self, restore_iter=None):
         if restore_iter is None:
             restore_iter = self.restore_iter
-        self.encoder.load_state_dict(torch.load(osp.join(
-            'checkpoint',
-            '{}-{:0>5}-encoder.ptm'.format(self.save_name, restore_iter))))
+        self.encoder.load_state_dict(
+            torch.load(
+                osp.join(
+                    "checkpoint",
+                    "{}-{:0>5}-encoder.ptm".format(self.save_name, restore_iter),
+                )
+            )
+        )
         opt_path = osp.join(
-            'checkpoint',
-            '{}-{:0>5}-optimizer.ptm'.format(self.save_name, restore_iter))
+            "checkpoint", "{}-{:0>5}-optimizer.ptm".format(self.save_name, restore_iter)
+        )
         if osp.isfile(opt_path):
             self.optimizer.load_state_dict(torch.load(opt_path))
 
--- tri_2d_net/pred.py
+++ tri_2d_net/pred.py
@@ -4,16 +4,26 @@
 
 import argparse
 import os
-
+import sys
 import numpy as np
+
+sys.path.append("./")
 
 from tri_2d_net.init_model import init_model
 
-parser = argparse.ArgumentParser(description='Prediction')
-parser.add_argument('--iter', default='8000', type=int,
-                    help='iter: iteration of the checkpoint to load. Default: 8000')
-parser.add_argument('--path', default='./demos/Positive_CAC_1.npy', type=str,
-                    help='path: path of the input image. Default: ./demos/Positive_CAC_1.npy')
+parser = argparse.ArgumentParser(description="Prediction")
+parser.add_argument(
+    "--iter",
+    default=8000,
+    type=int,
+    help="iter: iteration of the checkpoint to load. Default: 8000",
+)
+parser.add_argument(
+    "--path",
+    default="./demos/Positive_CAC_1.npy",
+    type=str,
+    help="path: path of the input image. Default: ./demos/Positive_CAC_1.npy",
+)
 opt = parser.parse_args()
 
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
@@ -21,4 +31,4 @@ os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 m = init_model()
 m.load_model(opt.iter)
 data = np.load(opt.path)
-print('Estimated CVD Risk:', m.aug_transform(data)[1])
+print("Estimated CVD Risk:", m.aug_transform(data)[1])
